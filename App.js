21d32ffb6dac4ab8a875553c16fa7c121e5284a5
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-04-18 01:25:47 +0500

    registration form

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import RegistationScreen from "./assets/Screens/RegistrationScreen";
+// SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RegistationScreen />
+    // <View style={styles.container}>
+    //   <Text style={styles.text}>FUCKING REACT-NATIVE!!!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "red",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
---
+++ assets/Screens/RegistrationScreen.js
@@ -0,0 +1,186 @@
+import { useState } from "react";
+import {
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  Keyboard,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  Pressable,
+  StatusBar,
+} from "react-native";
+
+export default function RegistationScreen() {
+  const [isFocused, setIsFocused] = useState(false);
+
+  function handleFocus() {
+    setIsFocused(true);
+  }
+
+  function handleBlur() {
+    setIsFocused(false);
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={"dark-content"} />
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ImageBackground
+          style={styles.backgroundImage}
+          source={require("../images/BG.jpg")}
+        >
+          <KeyboardAvoidingView
+            style={styles.formContainer}
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+          >
+            <Text style={styles.title}>Registration</Text>
+
+            <TextInput
+              placeholder="Login"
+              placeholderTextColor={"#BDBDBD"}
+              keyboardType={"default"}
+              style={[
+                styles.input,
+                { borderColor: isFocused ? "#FF6C00" : "#E8E8E8" },
+              ]}
+              onFocus={handleFocus}
+              onEndEditing={handleBlur}
+            />
+            <TextInput
+              placeholder="Email"
+              placeholderTextColor={"#BDBDBD"}
+              keyboardType={"email-address"}
+              style={[
+                styles.input,
+                { borderColor: isFocused ? "#FF6C00" : "#E8E8E8" },
+              ]}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor={"#BDBDBD"}
+              secureTextEntry={true}
+              keyboardType={"default"}
+              style={[
+                styles.inputPS,
+                { borderColor: isFocused ? "#FF6C00" : "#E8E8E8" },
+              ]}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+            <TouchableOpacity activeOpacity={0.4} style={styles.hint}>
+              <Text style={styles.hintText}>Show</Text>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.8} style={styles.button}>
+              <Text style={styles.buttonTitle}>Register</Text>
+            </TouchableOpacity>
+
+            <View style={styles.linkContainer}>
+              <TouchableOpacity>
+                <Text style={styles.link}>You already have account? </Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text style={styles.link}>Log in</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  formContainer: {
+    flex: 2 / 3,
+    width: "100%",
+    backgroundColor: "#FFFFFF",
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+  },
+  title: {
+    marginBottom: 32,
+    marginTop: 92,
+    fontWeight: 500,
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: "center",
+    letterSpacing: 0.16,
+    fontFamily: "Roboto-Medium",
+    color: "#212121",
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
+    height: 50,
+    padding: 16,
+    marginBottom: 16,
+    fontFamily: "Roboto-Regular",
+    color: "#212121",
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  inputPS: {
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    backgroundColor: "#F6F6F6",
+    height: 50,
+    padding: 16,
+    marginBottom: 43,
+    fontFamily: "Roboto-Regular",
+    color: "#212121",
+  },
+  hint: {
+    position: "absolute",
+    top: 307,
+    right: 32,
+  },
+  hintText: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+  button: {
+    backgroundColor: "#FF6C00",
+    paddingVertical: 16,
+    borderRadius: 100,
+    marginBottom: 16,
+  },
+  buttonTitle: {
+    textAlign: "center",
+    color: "#FFF",
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  linkContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  link: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+});
